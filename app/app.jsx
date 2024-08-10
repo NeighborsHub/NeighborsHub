9be3a2af5663943abdc0e73e624b3d27b5b477a6
s.hossein <s.hossein@rahkar.co>
2024-08-11 00:18:44 +0330

    adding theme

--- app/app.jsx
+++ app/app.jsx
@@ -3,16 +3,18 @@ import makeStore from "store/store";
 import { Provider } from "react-redux";
 import Bootstrap from "./bootstrap";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "styles/theme/theme";
+import theme from "styles/theme";
+import themeDark from "styles/theme-dark";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AxiosInterceptor } from "services/apiConfig";
 import withSnackbarNotifications from "notification";
 
 const App = ({ children }) => {
+  const darkMode = false;
   return (
     <GoogleOAuthProvider clientId="1092960991760-7ih57lpsujsf5at6toka7o0vd24ogm21.apps.googleusercontent.com">
       <Provider store={makeStore}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={darkMode ? themeDark : theme}>
           <AxiosInterceptor>
             <Bootstrap>{children}</Bootstrap>
           </AxiosInterceptor>
