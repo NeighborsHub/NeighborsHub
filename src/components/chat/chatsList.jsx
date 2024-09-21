38bf2f969e2c9e2558400db75bb65dcd2227c3aa
soheilhosseini <hosseini.soheil@yahoo.com>
2024-09-21 12:41:28 +0330

    1

--- src/components/chat/chatsList.jsx
+++ src/components/chat/chatsList.jsx
@@ -9,7 +9,7 @@ import { chatsSelector } from "store/slices/chatSlices";
 import { getMyChatsAction } from "store/actions/chatActions";
 import { useDispatch, useSelector } from "react-redux";
 import NavigationBar from "components/navigationBar/navigationBar";
-import ResponsiveHeader from 'components/header/responsiveHeader';
+import ResponsiveHeader from 'components/header/responsiveHeader.jsx';
 
 const ChatsList = ({ handleSetData }) => {
   const search = useInputHandler("");
